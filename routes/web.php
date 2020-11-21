64c184aca6b86a25066d4ec682d84e50cb5028fc
Norfatin <i.norfatin@gmail.com>
2020-11-21 10:56:19 +0800

    created simple and resourceful controller

--- routes/web.php
+++ routes/web.php
@@ -11,9 +11,9 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'StudentController@index');
+
+Route::get('/test', 'TestController@index');
 
 Route::get('/hello', function () {
     return "Hello World!";
